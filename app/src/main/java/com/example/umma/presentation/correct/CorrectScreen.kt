d9c2e22eddfd68d77e18ddb8d84716566226a108
woals <woals6318@gmail.com>
2026-05-09 19:16:06 +0900

    refactor/rename-correct-screen-to-feedback-list-screen

--- app/src/main/java/com/example/umma/presentation/correct/CorrectScreen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.umma.presentation.correct
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun CorrectScreen() {
-}
---
+++ app/src/main/java/com/example/umma/presentation/feed_back/FeedbackListScreen.kt
@@ -0,0 +1,26 @@
+package com.example.umma.presentation.feed_back
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun FeedbackListScreen() {
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text(
+                text = "교정 플레이스 홀더",
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
