2b9052755cab2e7cc2de7f0a7678ee0671868deb
woals <woals6318@gmail.com>
2026-05-09 19:16:34 +0900

    feat/navigation-setup-and-route-configuration

--- app/src/main/java/com/example/umma/MainActivity.kt
+++ app/src/main/java/com/example/umma/MainActivity.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.umma.presentation.live_chat.LiveChatScreen
-import com.example.umma.ui.theme.UmmaTheme
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.umma.core.navigation.Route
+import com.example.umma.presentation.chat.ChatScreen
+import com.example.umma.core.theme.UmmaTheme
+import com.example.umma.presentation.HomeScreen
+import com.example.umma.presentation.OnBoardingScreen
+import com.example.umma.presentation.auth.SignInScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,8 +23,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UmmaTheme {
-                LiveChatScreen()
+                ChatScreen()
             }
         }
     }
+}
+
+@Composable
+fun UmmaNavGraph() {
+    val rootNavController = rememberNavController()
+
+    NavHost(
+        navController = rootNavController,
+        startDestination = Route.OnBoarding
+    ) {
+
+        // 1. 온보딩
+        composable<Route.OnBoarding> {
+            OnBoardingScreen(
+                onNavigateToSignIn = { rootNavController.navigate(Route.SignIn) }
+            )
+        }
+
+        // 2. 로그인
+        composable<Route.SignIn> {
+            SignInScreen(
+                onNavigateToHome = { rootNavController.navigate(Route.Home) }
+            )
+        }
+
+        // 3. 대시보드
+        composable<Route.Home> {
+            HomeScreen(
+                onNavigateTo = { route ->
+                    rootNavController.navigate(route)
+                }
+            )
+        }
+
+    }
+}
+
+@Composable
+fun UmmaSubGraph() {
+    val childNavController = rememberNavController()
+    val navBackStackEntry = childNavController.currentBackStackEntry
+    val currentDestination = navBackStackEntry?.destination
 }
