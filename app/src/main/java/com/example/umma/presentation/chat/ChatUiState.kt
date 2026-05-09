78552516513fe3166643fcd2a984b622969f9a7f
woals <woals6318@gmail.com>
2026-05-09 19:15:45 +0900

    rename LiveChatUiState to ChatUiState

--- app/src/main/java/com/example/umma/presentation/chat/ChatUiState.kt
+++ app/src/main/java/com/example/umma/presentation/chat/ChatUiState.kt
@@ -1,8 +1,8 @@
-package com.example.umma.presentation.live_chat
+package com.example.umma.presentation.chat
 
 import com.example.umma.domain.model.AIState
 
-data class LiveChatUiState (
+data class ChatUiState (
     val isChatting: Boolean = false,
     val aiState: AIState = AIState.IDLE,
     val lastTranscription: String = "",
