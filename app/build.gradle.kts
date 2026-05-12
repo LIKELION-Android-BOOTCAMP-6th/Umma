cc6d378e47f247a38a1e02371c90292c7383c0f9
박재민1 <woals6318@gmail.com>
2026-05-13 00:43:35 +0900

    [Common] PageIndicator 공통 컴포넌트 구현  (#41)
    * [Chore] gitignore 업데이트
    
    ### 요약
    - 필요 없는 파일들 gitignore에 추가 포함
    
    ### 상세 내용
    - 없음
    
    ### 관련 이슈
    - 없음
    
    * [Fix] 아이콘 xml 파일 추가 및 NavItem.kt의 필드명과 필드 타입 변경
    
    ### 요약
    - NavItem.kt의 필드명과 타입 변경
    
    ### 상세 내용
    - 상세1: Figma의 아이콘 중 Icons 클래스에 존재하지 않는 아이콘을 xml 파일로 변경하여 필드명과 타입 변경
    
    - 핵심변경: NavItem.kt의 필드 기존 icon: imageVector -> iconRes: Int, build.gradle.kts에 material 라이브러리 추가, libs.versions.toml에 카탈로그 추가
    
    ### 관련 이슈
    - 없음
    
    * [Common] 온보딩의 하단 PageIndicator 구현
    
    ### 요약
    - 애니메이션 가능한 공통 페이지인디케이터 구현
    
    ### 상세 내용
    - 상세1: 온보딩과 추후 페이지들에 활용할 페이지 인디케이터 필요
    - 상세2: 핵심 인자: totalCount:Int(페이지 수), currentIndex(현재 보여지는 페이지 인덱스)
    - 상세3: 사용방법: 보여줘야할 페이지 수를 인자에 입력(기본 3페이지), currentIndex는 변하므로 관련 로직으로 currentIndex를 업데이트
    - 핵심변경: PageIndicator.kt (추가)
    
    ### 관련 이슈
    - #8, #9 (온보딩 화면 관련)
    
    * [Common] 바텀네비게이션바 Figma 디자인 적용
    ### 요약
    - 피그마의 디자인 내용에 따라 바텀네비게이션 바에 디자인을 적용함
    
    ### 상세 내용
    - 상세1: 미리 디자인을 볼 수 있도록 Preview 함수 작성
    - 상세2: 피그마의 색상과 TextStyle을 그대로 적용함
    - 핵심변경: UmmaBottomAppBar.kt
    
    ### 관련 이슈
    - Resolves: #19
    
    * [Common] 공통 다이얼로그 컴포넌트 구현
    
    ### 요약
    - 피그마의 디자인에 따라 전역에 쓰일 다이얼로그 컴포넌트를 구현함
    
    ### 상세 내용
    - 상세1: 앱 전역에 걸쳐 다이얼로그가 필요하므로 Dialog 컴포넌트를 활용해 커스텀 다이얼로그를 구현함
    - 상세2: 피그마의 색상과 TextStyle을 그대로 적용함
    - 핵심변경: UmmaDialog.kt (추가)
    
    ### 관련 이슈
    - Resolves: -
    
    * [Chore] gitignore 업데이트 (#39) (#42)
    
    ### 요약
    - 필요 없는 파일들 gitignore에 추가 포함
    
    ### 상세 내용
    - 없음
    
    ### 관련 이슈
    - 없음

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material)
     implementation(libs.compose.icons.extended)
+    implementation(libs.compose.icons)
 
     // Hilt
     implementation(libs.hilt.android)
---
+++ app/src/main/java/com/example/umma/core/ui/component/UmmaDialog.kt
@@ -0,0 +1,147 @@
+package com.example.umma.core.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.example.umma.core.theme.BackgroundPrimary
+import com.example.umma.core.theme.ButtonDialogSB
+import com.example.umma.core.theme.TextPrimary
+import com.example.umma.core.theme.ThemePrimary
+import com.example.umma.core.theme.TitleDialogSB
+
+// 움마 공통 다이얼로그 위젯
+@Composable
+fun UmmaDialog(
+    title: String,
+    modifier: Modifier = Modifier,
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
+    confirmText: String = "확인",
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Dialog(onDismissRequest = { }) {
+        Surface(
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            shape = RoundedCornerShape(30.dp),
+            color = BackgroundPrimary,
+            shadowElevation = 10.dp
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // 상단 취소 버튼
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    IconButton(
+                        onClick = { onCancel() },
+                        modifier = Modifier.align(Alignment.TopEnd )
+                    ) {
+                        Icon(
+                            Icons.Outlined.Cancel,
+                            contentDescription = "취소 버튼",
+                            modifier = Modifier.size(30.dp),
+                            tint = TextPrimary,
+                        )
+                    }
+                }
+
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = title,
+                        style = TitleDialogSB,
+                        color = TextPrimary,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+                //내부 컨텐츠
+                content()
+                // 하단 버튼
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Button(
+                    onClick = onConfirm,
+                    modifier = Modifier
+                        .width(92.dp)
+                        .height(48.dp)
+                        .padding(bottom = 16.dp),
+                    shape = RoundedCornerShape(24.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = ThemePrimary)
+                ) {
+                    Text(
+                        text = confirmText,
+                        style = ButtonDialogSB
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, )
+@Composable
+fun UmmaPreview() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundPrimary),
+        contentAlignment = Alignment.Center
+    ) {
+        UmmaDialog(
+            title = "학습 언어 선택",
+            onCancel = { print("") },
+            onConfirm = { },
+            modifier = Modifier.padding(horizontal = 32.dp),
+            confirmText = "확인",
+        ) {
+
+            val languages = listOf("영어", "한국어", "일본어", "중국어", "독일어")
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "하이",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
