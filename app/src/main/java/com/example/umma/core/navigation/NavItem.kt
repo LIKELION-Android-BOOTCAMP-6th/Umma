98d39074a3afef119ddb91bcf7d4885e8258c2a3
woals <woals6318@gmail.com>
2026-05-13 00:16:56 +0900

    [Fix] 아이콘 xml 파일 추가 및 NavItem.kt의 필드명과 필드 타입 변경
    ### 요약
    - NavItem.kt의 필드명과 타입 변경
    
    ### 상세 내용
    - 상세1: Figma의 아이콘 중 Icons 클래스에 존재하지 않는 아이콘을 xml 파일로 변경하여 필드명과 타입 변경
    
    - 핵심변경: NavItem.kt의 필드 기존 icon: imageVector -> iconRes: Int, build.gradle.kts에 material 라이브러리 추가, libs.versions.toml에 카탈로그 추가
    
    ### 관련 이슈
    - 없음

--- app/src/main/java/com/example/umma/core/navigation/NavItem.kt
+++ app/src/main/java/com/example/umma/core/navigation/NavItem.kt
@@ -1,5 +1,6 @@
 package com.example.umma.core.navigation
 
+import androidx.annotation.DrawableRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Analytics
 import androidx.compose.material.icons.filled.Book
@@ -7,55 +8,58 @@ import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Dashboard
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.RecordVoiceOver
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.example.umma.R
 
 sealed class NavItem(
     val route: Route,
     val label: String,
-    val icon: ImageVector
+    @field:DrawableRes val iconRes: Int
 ) {
     // 학습 탭
     object Study: NavItem(
         route = Route.StudyList,
         label = "학습",
-        icon = Icons.Default.Book
+        iconRes = R.drawable.import_contacts_24
     )
 
     // 홈 탭
     object Dashboard: NavItem(
         route = Route.Dashboard,
-        label = "대시보드",
-        icon = Icons.Default.Dashboard
+        label = "홈",
+        iconRes = R.drawable.home_24
     )
 
     // 교정 탭
     object Feedback: NavItem(
         route = Route.FeedbackList,
-        label = "피드백",
-        icon = Icons.Default.Edit
+        label = "교정",
+        iconRes = R.drawable.wand_shine_24
     )
 
     // 챗 탭
     object Chat: NavItem(
         route = Route.Chat,
-        label = "챗",
-        icon = Icons.Default.Call
+        label = "대화",
+        iconRes = R.drawable.record_voice_over_24
     )
 
     // 통계 탭
     object Analytics: NavItem(
         route = Route.Analytics,
         label = "통계",
-        icon = Icons.Default.Analytics
+        iconRes = R.drawable.leaderboard_24
     )
 
     companion object {
         val list = listOf(
-            Dashboard,
-            Analytics,
             Chat,
             Feedback,
-            Study
+            Dashboard,
+            Study,
+            Analytics,
         )
     }
 }
